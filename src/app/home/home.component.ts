e84299e5ed66e3cb208beee25566194ba180b6b6
alec <alec.kaye@gmail.com>
2020-10-28 22:17:10 -0400

    undo everything, except removed animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,24 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+// import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('FadeIn', [
-      transition(':enter', [  
-        style({opacity: 0, offset: 1.0}),
-        animate('1.2s 600ms ease-in')
-      ]),
-    ]),
-    trigger('AnimateList', [
-      transition(':enter', [  
-        style({opacity: 0, transform: 'translateX(-75%)', offset: 1.0}),
-        animate('1.2s 600ms ease-in')
-      ]),
-    ])
-   ]
+  // animations: [
+  //   trigger('FadeIn', [
+  //     transition(':enter', [  
+  //       style({opacity: 0, offset: 1.0}),
+  //       animate('1.2s 600ms ease-in')
+  //     ]),
+  //   ]),
+  //   trigger('AnimateList', [
+  //     transition(':enter', [  
+  //       style({opacity: 0, transform: 'translateX(-75%)', offset: 1.0}),
+  //       animate('1.2s 600ms ease-in')
+  //     ]),
+  //   ])
+  //  ]
 })
 export class HomeComponent implements OnInit {
 
