81ee635680da72af4ce1162f0ba4faf9b478e272
alec <alec.kaye@gmail.com>
2021-03-27 14:29:04 -0400

    add animation to services-offered

--- src/app/services-offered/services-offered.component.ts
+++ src/app/services-offered/services-offered.component.ts
@@ -1,36 +1,25 @@
 // import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services-offered',
   templateUrl: './services-offered.component.html',
   styleUrls: ['./services-offered.component.scss'],
-  // animations: [
-  //   trigger('FadeIn', [
-  //     transition(':enter', [  
-  //       style({opacity: 0, offset: 1.0}),
-  //       animate('1.2s 600ms ease-in')
-  //     ]),
-  //   ]),
-  //   trigger('LeftToRight', [
-  //     transition(':enter', [  
-  //       style({opacity: 0, transform: 'translateX(-25%)', offset: 1.0}),
-  //       animate('1.2s 600ms ease-in')
-  //     ]),
-  //   ]),
-  //   trigger('BottomToTop', [
-  //     transition(':enter', [  
-  //       style({opacity: 0, transform: 'translateY(25%)', offset: 1.0}),
-  //       animate('1.2s 600ms ease-in')
-  //     ]),
-  //   ]),
-  //   trigger('RightToLeft', [
-  //     transition(':enter', [  
-  //       style({opacity: 0, transform: 'translateX(25%)', offset: 1.0}),
-  //       animate('1.2s 600ms ease-in')
-  //     ]),
-  //   ])
-  //  ]
+  animations: [
+    trigger('LeftToRight', [
+      transition(':enter', [  
+        style({opacity: 0, transform: 'translateX(-25%)'}),
+        animate('1.2s 600ms ease-in-out')
+      ]),
+    ]),
+    trigger('RightToLeft', [
+      transition(':enter', [  
+        style({opacity: 0, transform: 'translateX(25%)'}),
+        animate('1.2s 600ms ease-in-out')
+      ]),
+    ])
+   ]
 })
 export class ServicesOfferedComponent implements OnInit {
 
