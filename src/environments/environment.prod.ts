05eaa934a21005230b9df7ee1388fdea3473f5bd
alec <alec.kaye@gmail.com>
2022-10-03 00:27:02 -0400

    update token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   headers: {
-    Authorization: 'Token 9386499fa5551888af4e56ea396840c94ec6da2b'
+    Authorization: 'Token cb211d42a47ad2bf1b2f86c0a26f6abcbabcee8f'
   },
   recaptcha_site_key: '6LdNOJEaAAAAAKutWdTV49FctNVFWOKvmKDWc7ru',
   aleckayetion_api_url: 'https://aleckayetion-api.us.aldryn.io',
